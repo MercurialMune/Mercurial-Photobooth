87f276b6f0fbbb3fb3ea22171997a1f3eaa01e1e
Cyprian Munene <munenecyp@gmail.com>
2019-02-27 19:53:38 +0300

    create templates for all image categories and their views controllers

--- booth/views.py
+++ booth/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def welcome(request):
+    return render(request, 'welcome.html')
+
+
+def gallery(request):
+    return render (request, 'categories/all-images.html')
+
+
+def education(request):
+    return render (request, 'categories/education.html')
+
+
+def entertainment(request):
+    return render (request, 'categories/ entertainment.html'
+                   )
